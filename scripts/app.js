73a1db1e5698a7358a5d97bffd53e4d5ee8fb281
SimonN <65023744+Simon994@users.noreply.github.com>
2020-08-27 14:20:47 +0100

    basic grid from manual array, with data-appearance for styling

--- scripts/app.js
+++ scripts/app.js
@@ -3,12 +3,40 @@
 
 function init(){
 
-console.log('JS works!')
+
+  const grid = document.querySelector('.grid')
+  const cells = []
+  console.log(grid)
+
+  const inputArr = 
+  [['*','*','*','*','*','*','*','*','*','*'],
+    ['*','O','O','O','O','O','O','O','O','*'],
+    ['*','O','O','O','O','O','O','O','O','*'],
+    ['*','O','O','O','O','O','O','O','O','*'],
+    ['*','O','O','O','O','O','O','O','O','*'],
+    ['*','O','O','O','O','O','O','O','O','*'],
+    ['*','O','O','O','O','O','O','O','O','*'],
+    ['*','O','O','O','O','O','O','O','O','*'],
+    ['*','O','O','O','O','O','O','O','O','*'],
+    ['*','*','*','*','*','*','*','*','*','*']]
+
+
+
+
+  for (let y = 0; y < inputArr.length; y++){
+    for (let x = 0; x < inputArr[1].length; x++){
+      const cell = document.createElement('div')
+      cell.setAttribute('data-appearance',`${inputArr[x][y]}`)
+      grid.appendChild(cell)
+      cells.push(cell)
+    }
+  }
+  console.log(cells)
+  
 
 }
 
 
 
-
-
+//init()
 window.addEventListener('DOMContentLoaded', init)
